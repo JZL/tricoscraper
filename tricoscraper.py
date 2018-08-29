8727eb42a11d38c14cbdb83f94c60867f04b9f71
JZL <jonahmail1@gmail.com>
2018-08-29 01:10:36 -0400

    Transform scraped data for use in scheduler

--- tricoscraper.py
+++ tricoscraper.py
@@ -7,6 +7,9 @@ Last updated on 20180815
 Todo:
     * ssl connections for the scraping?
 
+To kill scraping midway through:
+    ps aux|grep python|grep tricoscraper|awk '{prinit $2}'|xargs kill
+
 """
 
 
@@ -16,10 +19,206 @@ import requests
 import itertools
 import urllib3 as urllib
 import multiprocessing as multi
+import re
+import json
 
 from tricoinfo import TricoInfo
 from tricoglobals import TRICO_URL, TRICO_PREFIX
+from pdb import set_trace
 
+
+def _TricoScraper_HTML_to_JSON(c, has_DT=False, DT_index=0):
+    """
+    Converts c, a dictionary with keys from the HTML scraper, and transform
+    into the dictionairy with keys needed for the scheduler.
+
+    has_DT allows datetime information to be added to the dict, if available.
+    
+    DT_index allows control over which DT is used (can be multiple)
+
+    TODO: Don't allow get's silent "" replacement for critical fields
+    """
+    new_dict = {
+        'name':     '{} {}'.format(c.get('subj', ""),c.get('num', "")),
+        "comment":  c.get('comment', ""),
+        "id":       c.get('CRN', ""),
+        "ref":      c.get('CRN', ""),
+        "subj":     "{} ({})".format(c.get('Department', ""), c.get('Subj', "")),
+        "num":      c.get('Num', ""),
+        "sec":      c.get('Sec', ""),
+        "title":    c.get('Course Title', ""),
+        "cred":     c.get('Credit', ""),
+        "dist":     c.get('DIST', ""),
+        "lim":      c.get('LIM', ""),
+        "instruct": c.get('Instructor', "STAFF"),
+        "rm":       c.get('Room Location', ""),
+       # Not in html
+       #"dpt":      "MLLD"
+       #"type":    "Language",
+    }
+    if has_DT:
+        new_dict["days"]  = c['DT'][DT_index]['day_str']
+        new_dict["time"]  = c['DT'][DT_index]['time_str']
+        new_dict["start"] = c['DT'][DT_index]['start_end_time'][0]
+        new_dict["end"]   = c['DT'][DT_index]['start_end_time'][1]
+        new_dict["dow"]   = c['DT'][DT_index]['dow']
+
+    return new_dict
+
+def _TricoScraper_collate_classes(classes):
+    """
+    Converts a list of classes (from the scraper) into a list of dicts for the scheduler front-end
+    This list has different items depending on if it's a normal item, one with 0 datetimes, one with multiple datetimes
+
+    TODO
+    cat out.json |sed "s/}, {/},\n{/g"|grep -Po '"[^"]*":'|sort|uniq -c|sort -nk1,1
+    check to make sure every page has the required/assumed th
+    230 "Lab Sections":
+    707 "Instructor":
+    729 "Miscellaneous Links":
+    795 "Additional Course Info":
+    795 "Campus":
+    795 "Course Title":
+    795 "Credit":
+    795 "Department":
+    795 "Registration ID":
+    795 "Room Location":
+    795 "Semester":
+    795 "Time And Days":
+    795 "url":
+
+    instructor is optional (STAFF) and lab section (TODO)
+    """
+
+    # List of dicts of class:
+    #  0: Normal classes
+    #  1: Classes with no times
+    #  2: 2nd time for classes with 2 times
+    collated_list = [{}, {}, {}]
+
+    # TODO: There are some classes which have 3+ meeting times
+    # (https://trico.haverford.edu/cgi-bin/courseguide/cgi-bin/search.cgi?Swarthmore+Fall_2018+THEA+S+022+01)
+    # But this is also unsupported by the xls and includes a comment already, so am not
+    # supporting for now. Will break for these classes bc will only support 2/3
+    # days but the one in the comment is of a random position
+    for c in classes:
+        if len(c['DT']) == 0:
+            collated_list[1][c['CRN']] = _TricoScraper_HTML_to_JSON(c)
+        elif len(c['DT']) == 1:
+            collated_list[0][c['CRN']] = _TricoScraper_HTML_to_JSON(c, has_DT=True, DT_index=0)
+        elif len(c['DT']) >= 2:
+            collated_list[0][c['CRN']] = _TricoScraper_HTML_to_JSON(c, has_DT=True, DT_index=0)
+            collated_list[2][c['CRN']] = _TricoScraper_HTML_to_JSON(c, has_DT=True, DT_index=1)
+
+    return collated_list
+
+def _TricoScraper_parse_datetime(dt):
+    """
+    Takes in a datetime string, dt, of the form `MWF 11:30am-12:20pm, ...` and
+    converts it into list of dictionaries with days (as a list of day indices),
+    and 24-hour time start/end
+
+    @input string dt of form `MWF 11:30am-12:20pm, ...`
+    @output array of parsed dates
+    """
+    outArray = []
+
+    # Some classes have comma separated multiple times
+    # If same day, elides the second date list
+    # A few random classes *start* with ", " which messes everything up so replace
+    dt_list = re.sub("^, ?", "", dt).split(",")
+
+    # First days is optional bc, if is same day, doesn't include
+    """
+        Input: `MWF 11:30am-12:20pm`
+        Group 1. `MWF `
+        Group 2. `MWF`
+        Group 3. `11`
+        Group 4. `30`
+        Group 5. `am`
+        Group 6. `12`
+        Group 7. `20`
+        Group 8. `pm`
+    """
+    # Complicated bash script but gives the list of all Time and Days (pipe
+    # through grep -o .|sort|uniq -c to see all characters used (shows rare
+    # Sunday and Saturday)
+    # js-beautify.js out.json|grep "Time And Days"|sed 's/^.*: "//g'|sed "s/^,
+    # //"|sed 's/",//'|sed "s/,/\n/g"|grep -v "^$"|grep '^[^0]'|awk '{print $1}'
+
+    day_to_num = {"M": 1,"T": 2,"W": 3,"TH": 4,"F": 5,"S": 6,"U": 0};
+
+    # Returns "MTWTHFSU" as the only valid chars for days
+    days_chars = "".join(day_to_num.keys())
+
+    datetime_re = '^((['+days_chars+']*) )?([0-9]{2}):([0-9]{2})([ap]m)-([0-9]{2}):([0-9]{2})([ap]m)$'
+
+
+    # Returns something like 'W|U|TH|T|S|M|F' which is a regex to match each
+    # day key. The important part is that TH is before T (with a reverse sort)
+    # so the regex will match both chars instead of short circuting to T
+    days_re = "|".join(sorted(day_to_num.keys(), reverse=True))
+
+    # Need to be out of the loop bc if the second time is on the same day,
+    # doesn't write it, so the 2nd time needs to inherit
+    days = None
+
+    for d in dt_list:
+        match = re.match(datetime_re, d)
+        if match is None:
+            raise ValueError("This date string is not parseable by the regex: '{}'".format(dt))
+
+        # Check if has no date and the previous day (if it was comma separated)
+        # didn't have one
+        if match.group(2) is None:
+            if days is None:
+                raise ValueError("This date string has no days (MWF, etc): '{}'->{}".format(dt, dd))
+            # Else: inherit from previous comma separated day
+        else:
+            days = match.group(2) 
+
+        days_list = re.findall(days_re, days)
+
+        # Sanity check that the days tokens found in days_list should contain
+        # all characters in the original string
+        if len(days) != len("".join(days_list)):
+                raise ValueError("This date string's days (MWF) was not parsed correctly: '{}'".format(dt))
+
+        days_number = [day_to_num[item] for item in days_list]
+
+
+        start_end_time = []
+        time_str = []
+
+        # 3,6 is the group index of both hours (see above regex)
+        for i in [3, 6]:
+            time_str.append("{}:{} {}".format(match.group(i),
+                match.group(i+1), match.group(i+2)))
+
+            # 2 groups down from the hour is the am/pm
+            am_pm = match.group(i+2)
+            hour = int(match.group(i))
+
+            if am_pm == "pm":
+                # To add 12 to pm hours, need to make 12pm -> 0 before adding
+                # 12 (military time is 0 indexed)
+                if hour == 12:
+                    hour = 0
+                hour = hour+12
+            # 1 group down from the hour is the minutes
+            start_end_time.append("{}:{}".format(hour, match.group(i+1)))
+
+        outArray.append({
+            'time_str': "-".join(time_str),
+            'day_str': " ".join(days_list),
+            # TODO should not need to be a string but used with JSON.parse in
+            # scheduler
+            'dow': str(days_number),
+            'start_end_time': start_end_time
+        })
+
+    return outArray
+        
 
 def _TricoScraper_get_links(params):
     """Gets list of links given the parameters for the GET request.
@@ -58,7 +257,7 @@ def _TricoScraper_get_course(url):
     # Assumes "DIST" is at the end of the line
     additional_info_keys = {
         'CRN': 'CRN: ([0-9]*)',
-        'ENR': 'ENR LIM: ([0-9]*)',
+        'LIM': 'ENR LIM: ([0-9]*)',
         'CUR': 'CUR ENR: ([0-9]*)',
         'DIST': 'DIST: (.*)',
     }
@@ -81,9 +280,19 @@ def _TricoScraper_get_course(url):
                 m = re.search(additional_info_keys[k], value) 
                 if m is not None:
                     course[k] = m.group(1)
+                else:
+                    course[k] = ""
             if len(split_info) == 2:
-                course['comment'] = split_info[1]
+                course['comment'] = " ".join(split_info[1:])
+            else:
+                course['comment'] = ''
 
+    # Parse time and Days into start, end, days
+    if course['Time And Days'] == '':
+        course['DT'] = []
+    else:
+        course['DT'] = _TricoScraper_parse_datetime(course['Time And Days'])
+    course['Subj'], course['Num'], course['Sec'] = course['Registration ID'].split()
     course['URL'] = url
     return course
 
@@ -190,12 +399,36 @@ class TricoScraper(object):
         courses = self._pool.map(_TricoScraper_get_course, links)
         return courses
 
+def read_from_cache():
+    """
+    If don't want to scrape, but want to generated collated scraped data
+    """
+    f = open("out_scraped.json", "r")
+    inp = json.load(f)
+
+    res = json.dumps(_TricoScraper_collate_classes(inp))
+    f = open("out_collate.json", "w+")
+    f.write(res)
 
 def main():
-    import json
     ts = TricoScraper()
-    print(json.dumps(ts.search(smstr=['Fall_2018'])))
 
+    # Note: Bryn mawr/haverford have differnt
+    # db structures (i.e. bryn mawr has
+    # "Class Number" not CRN) so the collation
+    # for the scheduler will not work
+    ts_search = ts.search(smstr=['Fall_2018'], campus=["Swarthmore"])
+    result = json.dumps(ts_search)
+
+    f = open("out_scraped.json", "w+")
+    f.write(result)
+
+    read_cache()
 
 if __name__ == '__main__':
-    main()
+    # For debugging can use cache to not scrape
+    make_new = True
+    if make_new:
+        main()
+    else:
+        read_cache()
